655bcf88d8dbe49881cd3f4d3974817156e02def
José Antonio Bejarano Sigüenza <96siguenza@gmail.com>
2024-04-02 16:40:01 +0200

    Duplicar padre e hijo

--- Duplicador/Duplicador3/app.js
+++ Duplicador/Duplicador3/app.js
@@ -8,7 +8,7 @@ AFRAME.registerComponent('duplicador', {
 });
 
 function crearElementoEncima(padre) {
-    var tipoElemento = padre.tagName.toLowerCase(); // Obtener el tipo de elemento en minúsculas
+    var tipoElemento = padre.tagName;
 
     // Verificar si el elemento padre ya tiene un hijo
     if (padre.children.length > 0) {
@@ -16,10 +16,10 @@ function crearElementoEncima(padre) {
         var padreClon = padre.cloneNode(false);
         var hijoClon = padre.children[0].cloneNode(true);
 
-        //Ajustar la posición del clon para que se pinte encima del original
+        //Ajustar la posición de los clones
     padreClon.setAttribute('position', {
         x: parseFloat(padre.getAttribute('position').x),
-        y: parseFloat(padre.getAttribute('position').y) + 2, // Mover 2 unidades arriba
+        y: parseFloat(padre.getAttribute('position').y) + 2, 
         z: parseFloat(padre.getAttribute('position').z)
     });
     hijoClon.setAttribute('position', {
@@ -28,17 +28,16 @@ function crearElementoEncima(padre) {
         z: parseFloat(hijoClon.getAttribute('position').z)
     });
 
-        // Agregar los clones al lado del original
         padre.parentNode.appendChild(padreClon);
         padreClon.appendChild(hijoClon);
     } else {
-        // Crear un nuevo elemento del mismo tipo
+        // Si no tiene un hijo, simplemente agregar el nuevo elemento como hijo
         var nuevoElemento = document.createElement(tipoElemento);
         nuevoElemento.setAttribute('position', '1 0 0');
         nuevoElemento.setAttribute('rotation', {x: 0, y: 0, z: 0});
         nuevoElemento.setAttribute('color', '#FFC65D');
 
-        // Si no tiene un hijo, simplemente agregar el nuevo elemento como hijo
+        
         padre.appendChild(nuevoElemento);
     }
 }
