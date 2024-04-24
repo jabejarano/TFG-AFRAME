74fb61c8babd9a75708bab8a48487bfe3c178cf5
José Antonio Bejarano Sigüenza <96siguenza@gmail.com>
2024-04-24 10:32:01 +0200

    añadir sonido

--- Ejercicios_componentes/Ejercicio1/app.js
+++ Ejercicios_componentes/Ejercicio1/app.js
@@ -2,8 +2,16 @@
 // Registrar componente
 AFRAME.registerComponent('cambiador', {
     init: function () {
-        this.el.addEventListener('click', () => {
-            this.el.setAttribute('color', '#FF0000');  // Cambiar color a rojo
+        // this.el --> entidad actual a la que se le ha adjuntado el componente
+        var el = this.el;
+        var clickSound = document.getElementById('clickSound');
+        el.addEventListener('click', () => {
+            el.setAttribute('color', '#FF0000');  // Cambiar color a rojo
+            if (clickSound) {
+                clickSound.play();  // Reproducir sonido
+            } else {
+                console.error("Error: No se encontró ningún elemento de audio con el ID 'clickSound'.");
+            }
         });
     }
 });
