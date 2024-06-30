90cd5c6008f6fd303e22b19c7f5de7445026ba19
José Antonio Bejarano Sigüenza <96siguenza@gmail.com>
2024-06-30 18:25:50 +0200

    mejora names en los componentes shelf y option

--- interfaz-usuario-creador-objetos/shelf.js
+++ interfaz-usuario-creador-objetos/shelf.js
@@ -7,14 +7,14 @@ AFRAME.registerComponent('shelf', {
   init: function () {
     var objects = this.data.objects;
     var names = this.data.names;
-    var el = this.el;
 
     if (objects.length !== names.length) {
       console.error('La cantidad de objetos y nombres no coincide');
       return;
     }
 
-    // Almacena las figuras permitidas en el elemento shelf para acceso global
+    // Almacena las figuras y nombres permitidos en el elemento shelf para acceso global
     this.el.setAttribute('allowed-objects', objects.join(','));
+    this.el.setAttribute('allowed-names', names.join(','));
   }
 });
